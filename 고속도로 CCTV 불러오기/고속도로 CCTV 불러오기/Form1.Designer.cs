be73f32002e23d4ac58c2580af198414b998658c
asm6788 <asm6788@naver.com>
2016-08-05 21:20:05 +0900

    소스 업로드

---
+++ 고속도로 CCTV 불러오기/고속도로 CCTV 불러오기/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace 고속도로_CCTV_불러오기
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(610, 255);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(374, 247);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(497, 247);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_1);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "고속도로 이름";
+            this.columnHeader1.Width = 123;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "고속도로 구간";
+            this.columnHeader2.Width = 117;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "암호화된 TEXT";
+            this.columnHeader3.Width = 187;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "채널";
+            this.columnHeader4.Width = 66;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(516, 13);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(468, 23);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(515, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(622, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "최대 제한";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(516, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 542);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 고속도로 CCTV 불러오기/고속도로 CCTV 불러오기/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 고속도로_CCTV_불러오기
+{
+    public partial class Form1 : Form
+    {
+        int 제한 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        void CCTV수신()
+        { 
+
+            List<string> 수신된CCTV = new List<string>();
+            List<string> 수신된CCTVID = new List<string>();
+            for (int i = 10; 수신된CCTV.Count != 제한; i++)
+            {
+                수신된CCTV = 수신된CCTV.Distinct().ToList();
+                string strUri = "https://m.map.naver.com/traffic/cctvPlayer.nhn?cctvGroupId=17&channel=" + i + "&seq=2#";
+
+                /////////////////////////////////////////////////////////////////////////////////////
+                /* POST */
+                // HttpWebRequest 객체 생성, 설정
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(strUri);
+                request.Method = "POST";    // 기본값 "GET"
+                request.ContentType = "application/x-www-form-urlencoded";
+                // 요청, 응답 받기
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // 응답 Stream 읽기
+                Stream stReadData = response.GetResponseStream();
+                StreamReader srReadData = new StreamReader(stReadData, Encoding.UTF8);
+
+                // 응답 Stream -> 응답 String 변환
+                string strResult = srReadData.ReadToEnd();
+                string[] test = strResult.Split('\n');
+                string findThisString = "<h1>";
+                int strNumber;
+                int strIndex = 0;
+                for (strNumber = 0; strNumber < test.Length; strNumber++)
+                {
+                    strIndex = test[strNumber].IndexOf(findThisString);
+                    if (strIndex >= 0)
+                        break;
+                }
+                if (strNumber != 18 && test[strNumber] != "\t\t<h1><em class=\"_sectionName\"></em></h1>")
+                {
+                    string 고속도로이름 = test[strNumber].Remove(test[strNumber].IndexOf("<em"));
+                    고속도로이름 = 고속도로이름.Remove(0, 고속도로이름.IndexOf(">") + 1);
+                    string 고속도로구간 = test[strNumber].Remove(0, test[strNumber].IndexOf('\u0022' + ">"));
+                    고속도로구간 = 고속도로구간.Remove(0, 2);
+                    고속도로구간 = 고속도로구간.Remove(고속도로구간.IndexOf("<"));
+                    Console.WriteLine(strUri);
+                    if (수신된CCTV.Count != 0)
+                    {
+                        bool 있다 = false;
+                        for (int ii = 0; ii != 수신된CCTV.Count; ii++)
+                        {
+                            if (수신된CCTV[ii].Split(':')[0].Trim() + " : " + 수신된CCTV[ii].Split(':')[1].Trim() == 고속도로이름.Trim() + " : " + 고속도로구간.Trim())
+                            {
+
+                                있다 = true;
+                                수신된CCTV.RemoveAt(ii);
+                                ii--;
+                            }
+                            else
+                            {
+                                있다 = false;
+                            }
+                        }
+                        if (!있다)
+                        {
+                            있다 = false;
+                            if (test[170] != "\tvar cctv = {")
+                            {
+                                string 파싱값 = 고속도로이름 + " : " + 고속도로구간 + " : " + test[170].Remove(0, test[170].IndexOf(":") + 3).Remove(64) + " : " + test[171].Remove(0, test[171].IndexOf(":") + 3).Remove(test[171].Remove(0, test[171].IndexOf(":") + 3).IndexOf(",") - 1);
+                                수신된CCTV.Add(파싱값);
+                                Console.WriteLine(파싱값);
+
+                            }
+                            //else if (test[171].Remove(0, test[170].IndexOf(":") + 3).Remove(64).Equals(@"encryptedString\"))
+                            //{
+                            //    string 파싱값 = 고속도로이름 + " : " + 고속도로구간 + " : " + test[172].Remove(0, test[172].IndexOf(":") + 3).Remove(64) + " : " + test[173].Remove(0, test[173].IndexOf(":") + 3).Remove(test[173].Remove(0, test[173].IndexOf(":") + 3).IndexOf(",") - 1);
+                            //    수신된CCTV.Add(파싱값);
+                            //    Console.WriteLine(파싱값);
+                            //}
+                            else
+                            {
+                                string 파싱값 = 고속도로이름 + " : " + 고속도로구간 + " : " + test[171].Remove(0, test[171].IndexOf(":") + 3).Remove(64) + " : " + test[172].Remove(0, test[172].IndexOf(":") + 3).Remove(test[172].Remove(0, test[172].IndexOf(":") + 3).IndexOf(",") - 1);
+                                수신된CCTV.Add(파싱값);
+                                Console.WriteLine(파싱값);
+                            }
+                        }
+
+                    }
+                    else
+                    {
+
+                        if (test[170] != "\tvar cctv = {")
+                        {
+                            string 파싱값 = 고속도로이름 + " : " + 고속도로구간 + " : " + test[170].Remove(0, test[170].IndexOf(":") + 3).Remove(64) + " : " + test[171].Remove(0, test[171].IndexOf(":") + 3).Remove(test[171].Remove(0, test[171].IndexOf(":") + 3).IndexOf(",") - 1);
+                            수신된CCTV.Add(파싱값);
+                            Console.WriteLine(파싱값);
+
+                        }
+                        //else if (test[171].Remove(0, test[170].IndexOf(":") + 3).Contains("encryptedString"))
+                        //{
+                        //    string 파싱값 = 고속도로이름 + " : " + 고속도로구간 + " : " + test[171].Remove(0, test[170].IndexOf(":") + 3).Remove(64) + " : " + test[172].Remove(0, test[172].IndexOf(":") + 3).Remove(test[172].Remove(0, test[172].IndexOf(":") + 3).IndexOf(",") - 1);
+                        //    수신된CCTV.Add(파싱값);
+                        //    Console.WriteLine(파싱값);
+                        //}
+                        else
+                        {
+                            string 파싱값 = 고속도로이름 + " : " + 고속도로구간 + " : " + test[171].Remove(0, test[171].IndexOf(":") + 3).Remove(64) + " : " + test[172].Remove(0, test[172].IndexOf(":") + 3).Remove(test[172].Remove(0, test[172].IndexOf(":") + 3).IndexOf(",") - 1);
+                            수신된CCTV.Add(파싱값);
+                            Console.WriteLine(파싱값);
+                        }
+                    }
+
+                    Invoke(new Action(
+                           delegate ()
+                           {
+                               progressBar1.Value = Convert.ToInt32(Convert.ToSingle(수신된CCTV.Count) / Convert.ToSingle(제한) * 100);
+                               label1.Text = 수신된CCTV.Count + " / " + 제한;
+                           }));
+
+                    고속도로이름 = null;
+                    고속도로구간 = null;
+
+                
+                }
+            }
+            Invoke(new Action(
+                           delegate ()
+                           {
+                               listView1.Items.Clear();
+                           }));
+            foreach (string 입력 in 수신된CCTV)
+            {
+                string[] row = { 입력.Split(':')[0].Trim(), 입력.Split(':')[1].Trim(), 입력.Split(':')[2].Trim(), 입력.Split(':')[3].Trim() };
+                var listViewItem = new ListViewItem(row);
+                Invoke(new Action(
+                           delegate ()
+                           {
+                               listView1.Items.Add(listViewItem);
+                           }));
+
+                Console.WriteLine(입력);
+            }
+
+
+        }
+
+       
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem item = listView1.SelectedItems[0];
+                axWindowsMediaPlayer1.URL = "http://cctvsec.ktict.co.kr/"+ item.SubItems[3].Text + "/"+ item.SubItems[2].Text;
+            }
+         
+        }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == 1)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+           
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                제한 = Convert.ToInt32(textBox1.Text);
+                Thread thread = new Thread(new ThreadStart(CCTV수신));
+                thread.Start();
+            }
+            else
+            {
+                MessageBox.Show("제한값좀입력좀....");
+            }
+        }
+    }
+    
+}
